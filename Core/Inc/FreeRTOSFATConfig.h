83b3b79fdf1cda58070f61ba381ded916d4be482
Edoardo Bortolozzo <edoardobortolozzo@gmail.com>
2023-11-27 13:25:55 +0100

    Added FreeRTOSFATConfig.h

---
+++ Core/Inc/FreeRTOSFATConfig.h
@@ -0,0 +1,13 @@
+/*
+ * FreeRTOSFATConfig.h
+ *
+ *  Created on: 27 nov 2023
+ *      Author: edoardo
+ */
+
+#ifndef INC_FREERTOSFATCONFIG_H_
+#define INC_FREERTOSFATCONFIG_H_
+
+
+
+#endif /* INC_FREERTOSFATCONFIG_H_ */
