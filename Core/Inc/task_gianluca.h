442762ce2448b68475a2279dc337927e8cff1dc3
gp-9 <118211036+gp-9@users.noreply.github.com>
2023-12-04 16:27:38 +0100

    Trying to setup RAM disk

---
+++ Core/Inc/task_gianluca.h
@@ -0,0 +1,17 @@
+#ifndef TAKS_GIANLUCA_H_
+#define TAKS_GIANLUCA_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "ff_stdio.h"
+#include "ff_headers.h"
+
+void vTaskGianluca(void *pvParameters);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TAKS_GIANLUCA_H_
