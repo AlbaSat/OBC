5320a0796d4f8b7a2e915c1cff5546c0eeb76805
alebal123bal <balzanalessandro2001@gmail.com>
2024-04-06 13:44:32 +0200

    Refactored timer setup

--- Core/Src/timer_utils.c
+++ Core/Src/timer_utils.c
@@ -6,15 +6,16 @@
 
 //Set up Timer 1, the Advanced Control Timer, in order to use it with the ultra sonic Sensor
 void setup_Timer1(){
-	RCC->AHB1ENR	|=	RCC_AHB1ENR_GPIOAEN; 						//enable GPIOA Clock
-	GPIOA->MODER	|=	(1<<0);   									//set PA0 to Output
+	RCC->AHB1ENR	|=	RCC_AHB1ENR_GPIOAEN; 		//enable GPIOA Clock, page 117/844 reference
+	GPIOA->MODER	|=	((0x1UL) << (0U));   		//set PA0 to general purpose output, page 157/844
 
 	//configure Timer1 to generate microseconds delay
-	RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;  /*Enable TIM1 clock*/
-	TIM1->PSC = 16 -1;  /* 16 000 000 /16 = 1000 000*/
-	TIM1->ARR = 1;  /* 1000 000 /1 = 1000000*/
-	TIM1->CNT =0;
-	TIM1->CR1 =1;
+	//page 287/844 reference
+	RCC->APB2ENR |= RCC_APB2ENR_TIM1EN; // Enable TIM1 clock, page 121/844 reference
+	TIM1->PSC = 16 - 1; // 16 000 000 / 16 = 1000 000, page 306/844 reference
+	TIM1->ARR = 0x0001; // 1000 000 / 1 = 1000000, page 306/844 reference
+	TIM1->CNT = 0; // page 306/844 reference
+	TIM1->CR1 = 1; //page 287/844 reference
 }
 
 
@@ -33,9 +34,10 @@ void delaymS(uint32_t ms) //delay for certain amount in milliseconds
 
 }
 
-void delayuS(uint32_t us) //delay for certain amount in microseconds
-	{
-	for(int i =0;i<us;i++){
+
+//Delay for certain amount in microseconds
+void delayuS(uint32_t us){
+	for(int i =0; i<us; i++){
 		while(!(TIM1->SR & 1)){
 
 		}   /*wait for UIF set*/
@@ -45,9 +47,10 @@ void delayuS(uint32_t us) //delay for certain amount in microseconds
 	}
 }
 
-uint32_t read_echo(uint32_t timeout)
-	{
-	uint32_t duration;
+uint32_t read_echo(uint32_t timeout){
+	volatile uint32_t duration = 0;
+
+	//Wait for pulse to arrive
 	while(!((GPIOA->IDR)&GPIO_IDR_ID1)){
 		duration++;
 		delayuS(1);
@@ -58,6 +61,7 @@ uint32_t read_echo(uint32_t timeout)
 
 	duration=0;
 
+	//Measure pulse duration
 	while((GPIOA->IDR&GPIO_IDR_ID1)){
 		duration++;
 		delayuS(1);
@@ -67,5 +71,4 @@ uint32_t read_echo(uint32_t timeout)
 	}
 
 	return duration;
-
-	}
+}
--- Core/Src/vTask2Ale.c
+++ Core/Src/vTask2Ale.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 #define Trig_high		GPIOA->BSRR=GPIO_BSRR_BS_0 			// turn on 	PA0 (trig pin)
-#define Trig_low		GPIOA->BSRR=GPIO_BSRR_BR_0 			// turn off 	PA0 (trig pin)
+#define Trig_low		GPIOA->BSRR=GPIO_BSRR_BR_0 			// turn off PA0 (trig pin)
 
-uint32_t duration;
+volatile uint32_t dur=0;
 double distance;
 
 extern SemaphoreHandle_t printMutex;
@@ -17,14 +17,13 @@ void vTask2Ale(void *pvParameters){
 
     for(;;) {
 		Trig_low; 											//turn off trig
-		delayuS(10); 										//wait 10uS
 		Trig_high;  										//turn on trig
 		delayuS(10);
 		Trig_low;
-		duration=read_echo(400000); 						//measure the time of echo pin in us
-		distance=duration*0.1715*0.5;						//distance=duration/2*SOUND_SPEED in cm
+		dur = read_echo(400000); 			//measure the time of echo pin in us
+		distance = dur * 0.17 * 0.5;		//distance=duration/2*SOUND_SPEED
 		if(xSemaphoreTake(printMutex, (TickType_t)10) == pdTRUE) {
-		    FF_PRINTF("distance %f cm\r\n", distance);
+		    FF_PRINTF("Distance %f cm\r\n", distance);
 		    xSemaphoreGive(printMutex);
 		}
 		vTaskDelay(pdMS_TO_TICKS(100));
