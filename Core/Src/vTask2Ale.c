7afec7025f4c05fdc57961472d9aea6ea01fcf43
alebal123bal <balzanalessandro2001@gmail.com>
2024-04-13 20:00:15 +0200

    Removed final overhead of HAL functions; now ultrasound distance is perfect

--- Core/Src/vTask2Ale.c
+++ Core/Src/vTask2Ale.c
@@ -16,35 +16,34 @@ extern SemaphoreHandle_t printMutex;
 volatile void vTask2Ale(void *pvParameters){
 
     for(;;) {
-//		//Set TRIG to LOW for few us
-//		HAL_GPIO_WritePin(TRIG_GPIO_Port, TRIG_Pin, GPIO_PIN_RESET);
-//		delayuS(3);
+		//Set TRIG to LOW for few us
+		HAL_GPIO_WritePin(TRIG_GPIO_Port, TRIG_Pin, GPIO_PIN_RESET);
+		delayuS(3);
 
 		//HAL_GPIO_WritePin(TRIG_GPIO_Port, TRIG_Pin, GPIO_PIN_SET);
     	Trig_high;
 		delayuS(2);
 		//HAL_GPIO_WritePin(TRIG_GPIO_Port, TRIG_Pin, GPIO_PIN_RESET);
 		Trig_low;
-		delayuS(2);
-//
-//		//Wait for pulse to arrive
-//		while(HAL_GPIO_ReadPin(ECHO_GPIO_Port, ECHO_Pin) == GPIO_PIN_RESET);
-//
-//		//Start measuring pulse width in us
-//		numTicks = 0;
-//		while(HAL_GPIO_ReadPin(ECHO_GPIO_Port, ECHO_Pin) == GPIO_PIN_SET){
-//		  numTicks++;
-//		  delayuS(2);	//TODO measure this delay that each tick is taking; for now put 2.8
-//		}
-//
-//		//Estimate distance
-//		distance = (numTicks + 0.0f) * us_per_Tick * speed_of_sound;
-//		if(xSemaphoreTake(printMutex, (TickType_t)10) == pdTRUE) {
-//			FF_PRINTF("Distance %f cm\r\n", distance);
-//			xSemaphoreGive(printMutex);
-//		}
-//
-//		HAL_Delay(100);
+
+		// Wait for pulse to arrive
+		while(!(GPIOA->IDR & GPIO_PIN_1));
+
+		//Start measuring pulse width in us
+		numTicks = 0;
+		while(GPIOA->IDR & GPIO_PIN_1){
+		  numTicks++;
+		  delayuS(2);	//TODO measure this delay that each tick is taking; for now put 2.8
+		}
+
+		//Estimate distance
+		distance = (numTicks + 0.0f) * us_per_Tick * speed_of_sound;
+		if(xSemaphoreTake(printMutex, (TickType_t)10) == pdTRUE) {
+			FF_PRINTF("Distance %f cm\r\n", distance);
+			xSemaphoreGive(printMutex);
+		}
+
+		HAL_Delay(100);
     }
     vTaskDelete(NULL);
 }
